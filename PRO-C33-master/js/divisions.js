ea3f6c4ab30713aa12a089f109cad1186ea5df97
awesomecoder2318 <84549703+awesomecoder2318@users.noreply.github.com>
2021-08-09 15:03:42 -0600

    Add files via upload

---
+++ PRO-C33-master/js/divisions.js
@@ -0,0 +1,20 @@
+class Divisions{
+    constructor(x,y,w,h){
+    var options ={
+        isStatic: true
+    }
+    this.body = Bodies.rectangle(x,y,w,h,options);
+    this.w = w;
+    this.h = h;
+    World.add(world,this.body)
+    }
+    display(){
+    var pos = this.body.position;
+    push()
+    rectMode(CENTER);
+    fill("red");
+    rect(pos.x,pos.y,10,300,200,800);
+    pop()
+    }
+    
+    }
